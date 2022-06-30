a3e5afededba90ac9893e6deea5e1c53f4b684a3
Peanut Butter <avni0000patel@gmail.com>
2022-06-30 15:47:24 -0400

    color-coded time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,29 @@
 var today = moment();
 console.log(today);
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+// Each time block is color-coded to indicate whether it is in the past, present, or future
+function color() {
+    var currentTime = moment().hour();
+    console.log(currentTime);
+
+    $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(timeBlock);
+
+        // Remove classes
+        $(this).removeClass("present past future");
+
+        // Add classes
+        if (currentTime > timeBlock) {
+            $(this).addClass("past");
+        } else if (currentTime < timeBlock) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+
+    })
+}
+
+color();
